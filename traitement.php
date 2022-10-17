a2ffeb96eaba94567bd3dc3e0f2f1012739cac75
Orionas <100146226+Orionasis@users.noreply.github.com>
2022-10-17 17:20:07 +0200

    Add files via upload

---
+++ traitement.php
@@ -0,0 +1,8 @@
+<?php
+echo "bonjour à tous";
+$nom=$_POST['nom'];
+$prenom=$_POST['prenom'];
+$email=$_POST['email'];
+$pwd = $_POST['password'];
+echo "vous avez taper :".$nom." ".$prenom." ".$email." ".$pwd;
+?>
