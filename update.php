0ed31b95e6c09b327100e3dc28e0e89a930fbb20
Orionas <100146226+Orionasis@users.noreply.github.com>
2022-10-21 09:23:32 +0200

    Update update.php

--- update.php
+++ update.php
@@ -1,9 +1,6 @@
 <?php
 require("connection.php");
 $id=$_GET['id'];
-$nom="";
-$email="";
-$contenu="";
 $sql="update message set nom='coucou',email='coucou@gmail.com',contenu='coucou cc' where id='".$id."'";
 if ($conn->query($sql) === TRUE) {
     echo "Record updated successfully";
