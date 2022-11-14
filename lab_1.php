a7439ca65674f66f6025ee04146baeddab15ed14
Orionas <100146226+Orionasis@users.noreply.github.com>
2022-11-14 10:56:30 +0100

    Delete lab_1.php

--- lab_1.php
+++
@@ -1,7 +0,0 @@
-<?php
-echo "<h1 align=center> PHP Orienté Objet</h1>";
-echo "<h2> Part 1: Les classes, méthodes, attributs d'instance et de classe</h2>";
-echo "<h2> Part 2: Les modificateurs de visibilités</h2>";
-echo "<h2> Part 3: L'héritage</h2>";
-echo "<h2> Part 4: Les classes abstraites et interfaces</h2>";
-?>
