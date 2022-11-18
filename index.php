628fbb2ff3e136a534eea94b1b2363b5363a30c0
Orionasis <samy.hamdan.upmc@gmail.com>
2022-11-18 10:25:40 +0100

    ajout

---
+++ index.php
@@ -0,0 +1,67 @@
+<html>
+<head>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+</head>
+<body style="margin: 10px;padding:10px">
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Projet GK - PHP </a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Créer un compte</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Connexion</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+
+
+<h1 align="center">Bienvenue sur la boutique de GK</h1>
+
+
+<h3 align="center" style="padding-top:20px">Liste de nos produits</h3>
+
+
+<table class="table table-bordered">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Image</th>
+      <th scope="col">Libelle</th>
+      <th scope="col">Prix</th>
+      <th scope="col">Disponibilité</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Photo PC HP</td>
+      <td>PC HP i7 12 Go 512 SSD</td>
+      <td>2400</td>
+      <td>Disponible</td>
+    </tr>
+    <tr>
+    <th scope="row">1</th>
+      <td>Photo Samsung A33 5G</td>
+      <td>Smart Phone Samsung A33 5G</td>
+      <td>1900</td>
+      <td>Disponible</td>
+    </tr>
+
+  </tbody>
+</table>
+
+</body>
+
+</html>
+
+<?php
+?>
