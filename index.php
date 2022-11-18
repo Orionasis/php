9f611b3e4016338fc2052d3a808484890db7b86f
Orionasis <samy.hamdan.upmc@gmail.com>
2022-11-18 10:39:16 +0100

    ajout

--- index.php
+++ index.php
@@ -1,23 +1,24 @@
 <html>
 <head>
+<meta charset="UTF-8"/>
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 </head>
 <body style="margin: 10px;padding:10px">
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Projet GK - PHP </a>
+  <a class="navbar-brand" href="index.php">Projet GK - PHP </a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav">
       <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Créer un compte</a>
+        <a class="nav-link" href="signup.html">Créer un compte</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Connexion</a>
+        <a class="nav-link" href="signin.html">Connexion</a>
       </li>
     </ul>
   </div>
